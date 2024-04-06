062bcfc615630fbc67b50df9dc06b1b5c330d0b2
arvgan <arvgan@node0.arvgan-200465.utcs-aos-s24-pg0.utah.cloudlab.us>
2024-04-06 16:26:53 -0600

    Added initial code

---
+++ parser.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        printf("No program specified.\n");
+        exit(-1);
+    }
+
+    return 0;
+}
